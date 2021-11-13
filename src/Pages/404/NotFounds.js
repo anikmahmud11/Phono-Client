0c111f30f636dbe6f76d0b899e5d7b4b79411c45
Anik <baust.cse.160201078@gmail.com>
2021-11-13 23:36:42 +0600

    404

--- src/Pages/404/NotFounds.js
+++ src/Pages/404/NotFounds.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFounds = () => {
     return (
-        <div>
-            Not found
+        <div className="container">
+              <img className=" img-fluid" src="https://media3.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=ecf05e47t4thjv6y0e31zj8ressac01uchxw6lu978abduu1&rid=giphy.gif&ct=g" ></img>
         </div>
     );
 };
